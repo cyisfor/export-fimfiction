999a2796d7e67ed07052ea97150f4fa8f0909cfa
Cy <nope>
2015-07-21 23:04:49 -0700

    auto (/extra/user/creation/stories/code/export-fimfiction.d/source/gui.c) 51 91 1792

--- source/gui.c
+++ source/gui.c
@@ -1,19 +1,27 @@
 #include <gtk/gtk.h>
 #include <glib.h>
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h> // realloc
+
 extern void reloadFile(void*,void (*)(void));
 extern const char* getContents(int);
 
 static GtkLabel* makeLabel() {
-  GtkWidget* label = gtk_label_new("...");
-  gtk_label_set_justify(GTK_LABEL(label),GTK_JUSTIFY_LEFT);
-  g_object_widget_set_property(label,"halign",GTK_ALIGN_START);
-  g_object_widget_set_property(label,"margin-start", 3);
-  return GTK_LABEL(label);
+  GtkLabel* label = GTK_LABEL(gtk_label_new("..."));
+  gtk_label_set_justify(label,GTK_JUSTIFY_LEFT);
+  GValue value = G_VALUE_INIT;
+  g_value_init(&value, G_TYPE_INT);
+  g_value_set_int(&value,GTK_ALIGN_START);
+  g_object_set_property(G_OBJECT(label),"halign",&value);
+  g_value_set_int(&value,3);
+  g_object_set_property(G_OBJECT(label),"margin-start", &value);
+  return label;
 }
 
 GtkGrid* tbl = NULL;
-GtkCliboard* clip = NULL;
+GtkClipboard* clip = NULL;
 
 struct row {
   GtkButton* btn;
@@ -26,28 +34,30 @@ int nrows = 0;
 
 // from D getContents(i) => const char*
 static void on_click(GtkButton* button, gpointer udata) {
-  int i = (int) udata;
+  int i = (intptr_t) udata;
   gtk_clipboard_set_text(clip,getContents(i),-1);
 }
 
 // D calls assureRow/refreshRow when it's reloaded its stuff
 void assureRow(int i, const char* name) {
-  struct row* row = g_new(struct row);
   if(nrows<=i) {
     nrows = i + 1;
     rows = realloc(rows,sizeof(struct row) * nrows);
   }
   struct row* cur = rows + i;
-  if(row->ready) return;
+  if(cur->ready) return;
   gtk_grid_insert_row(tbl,i+1);
-  cur->btn = gtk_button_new_with_label(name);
-  g_signal_connect("clicked",on_click,(void*)i);
-  gtk_grid_attach(tbl,cur->btn,0,i+1,1,1);
+  cur->btn = GTK_BUTTON(gtk_button_new_with_label(name));
+  g_signal_connect(cur->btn,
+                   "clicked",
+                   G_CALLBACK(on_click),
+                   (void*)(intptr_t)i);
+  gtk_grid_attach(tbl,GTK_WIDGET(cur->btn),0,i+1,1,1);
   cur->label = makeLabel();
-  gtk_grid_attach(tbl,cur->label,1,i+1,1,1);
+  gtk_grid_attach(tbl,GTK_WIDGET(cur->label),1,i+1,1,1);
   cur->counter = makeLabel();
-  gtk_grid_attach(tbl,cur->counter,2,i+1,1,1);
-  row->ready = true;
+  gtk_grid_attach(tbl,GTK_WIDGET(cur->counter),2,i+1,1,1);
+  cur->ready = true;
 }
   
 void refreshRow(int i, const char* summary, const char* count) {
@@ -73,7 +83,7 @@ refreshPathChanged (GFileMonitor     *monitor,
                     GFile            *other_file,
                     GFileMonitorEvent event_type,
                     gpointer          udata) {
-  if(event == G_FILE_MONITOR_EVENT_CHANGES_DONE_HINT) {
+  if(event_type == G_FILE_MONITOR_EVENT_CHANGES_DONE_HINT) {
     //puts("updated");
     struct delegate* dg = (struct delegate*) udata;
     reloadFile(dg->ctx,dg->funcptr); // causes D to call assureRow/refreshRow for each row.
@@ -85,14 +95,14 @@ void guiLoop(const char* path, void* ctx, void (*funcptr)(void)) {
   struct delegate dg = { ctx, funcptr };
   gtk_init(NULL,NULL);
   GtkWidget* win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  tbl = gtk_grid_new();
-  gtk_window_add(win,tbl);
-  g_signal_connect(win,"destroy",gtk_main_quit);
+  tbl = GTK_GRID(gtk_grid_new());
+  gtk_container_add(GTK_CONTAINER(win),GTK_WIDGET(tbl));
+  g_signal_connect(win,"destroy",gtk_main_quit,NULL);
   clip = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
   gtk_grid_insert_row(tbl,0);
   GtkWidget* refreshbtn = gtk_button_new_with_label("Reload");
   gtk_grid_attach(tbl,refreshbtn,0,0,3,1);
-  g_signal_connect(refreshbtn,"clicked",doRefresh,&dg);
+  g_signal_connect(refreshbtn,"clicked",G_CALLBACK(doRefresh),&dg);
   GFile* f = g_file_new_for_path(path);
   GError* err = NULL;
   GFileMonitor* mon = g_file_monitor_file
