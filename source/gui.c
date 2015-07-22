8c3275f80d05bddd021cb932fbfb7fb8c4c127a2
Cy <nope>
2015-07-21 22:55:08 -0700

    auto (/extra/user/creation/stories/code/export-fimfiction.d/source/gui.c) 52 161 1916

--- source/gui.c
+++ source/gui.c
@@ -1,10 +1,14 @@
 #include <gtk/gtk.h>
+#include <glib.h>
+
+extern void reloadFile(void*,void (*)(void));
+extern const char* getContents(int);
 
 static GtkLabel* makeLabel() {
   GtkWidget* label = gtk_label_new("...");
-  gtk_label_set_justify(label,GTK_JUSTIFY_LEFT);
-  gtk_widget_set_property(label,"halign",GTK_ALIGN_START);
-  gtk_widget_set_property(label,"margin-start", 3);
+  gtk_label_set_justify(GTK_LABEL(label),GTK_JUSTIFY_LEFT);
+  g_object_widget_set_property(label,"halign",GTK_ALIGN_START);
+  g_object_widget_set_property(label,"margin-start", 3);
   return GTK_LABEL(label);
 }
 
@@ -26,6 +30,7 @@ static void on_click(GtkButton* button, gpointer udata) {
   gtk_clipboard_set_text(clip,getContents(i),-1);
 }
 
+// D calls assureRow/refreshRow when it's reloaded its stuff
 void assureRow(int i, const char* name) {
   struct row* row = g_new(struct row);
   if(nrows<=i) {
@@ -52,27 +57,51 @@ void refreshRow(int i, const char* summary, const char* count) {
   gtk_label_set_text(cur->counter, count);    
 }
 
+struct delegate {
+  void* ctx;
+  void (*funcptr)(void);
+};
+
 static void doRefresh(GtkButton* btn, void* udata) {
-  refreshRows(); // causes D to call assureRow/refreshRow for each row.
+  struct delegate* dg = (struct delegate*) udata;
+  reloadFile(dg->ctx,dg->funcptr); // causes D to call assureRow/refreshRow for each row.
 }
 
-static guint startup(void* udata) {
-  startMonitoring(); // and then... we need... the file descriptor from D? inode? path?
-  return FALSE;
+static void
+refreshPathChanged (GFileMonitor     *monitor,
+                    GFile            *file,
+                    GFile            *other_file,
+                    GFileMonitorEvent event_type,
+                    gpointer          udata) {
+  if(event == G_FILE_MONITOR_EVENT_CHANGES_DONE_HINT) {
+    //puts("updated");
+    struct delegate* dg = (struct delegate*) udata;
+    reloadFile(dg->ctx,dg->funcptr); // causes D to call assureRow/refreshRow for each row.
+  }
 }
 
-int guiLoop(void) {
+
+void guiLoop(const char* path, void* ctx, void (*funcptr)(void)) {
+  struct delegate dg = { ctx, funcptr };
   gtk_init(NULL,NULL);
   GtkWidget* win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   tbl = gtk_grid_new();
-  win.add(tbl);
-  win.connect("destroy",gtk_main_quit);
+  gtk_window_add(win,tbl);
+  g_signal_connect(win,"destroy",gtk_main_quit);
   clip = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
   gtk_grid_insert_row(tbl,0);
   GtkWidget* refreshbtn = gtk_button_new_with_label("Reload");
   gtk_grid_attach(tbl,refreshbtn,0,0,3,1);
-  g_signal_connect(refreshbtn,"clicked",doRefresh);
-  g_idle_add(startup,NULL);
+  g_signal_connect(refreshbtn,"clicked",doRefresh,&dg);
+  GFile* f = g_file_new_for_path(path);
+  GError* err = NULL;
+  GFileMonitor* mon = g_file_monitor_file
+    (path,
+     G_FILE_MONITOR_NONE,NULL,&err);
+  assert(err==NULL);
+  g_signal_connect(mon,"changed",refreshPathChanged,&dg);
+  gtk_widget_show_all(win);
   gtk_main();
+  exit(0);
 }
 
