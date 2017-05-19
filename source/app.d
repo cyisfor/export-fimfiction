7b2b347279e111a0bbd6f5f3768830d139977777
autocommit <autocommit>
2017-05-19 07:54:58 +0000

    auto 0 249 2920

--- source/app.d
+++ source/app.d
@@ -162,7 +162,7 @@ void main(string[] args)
 		import html_when: process_when;
 		import std.file: readText;
 		auto doc = createDocument(readText(source));
-		auto ref storyE = doc.root;
+		alias storyE = doc.root;
 		info(doc.root.html);
 		process_when(storyE);
 		infof("um",storyE.html);
