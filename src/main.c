f8c472c8c02550ac57986f60d3c5c23281ed8834
autocommit <autocommit>
2017-08-13 04:37:44 +0000

    auto 0 15 272

--- src/main.c
+++ src/main.c
@@ -233,10 +233,12 @@ void main(int argc, char** argv) {
 		html_when(storyE);
 		xmlNode* titleE = get_title(storyE);
 		if(titleE) {
+			xmlUnlinkNode(titleE);
 			INFO("Found title %s",titleE->children->content);
-			title.s = titleE->children->content;
-			// strdup?
-			title.l = strlen(title.s);
+			title.l = strlen(titleE->children->content);
+			title.s = realloc(title.s, title.l);
+			memcpy(title.s, titleE->children->content, title.l);
+			xmlFreeNode(titleE);
 		}
 		free(author.s);
 		author.s = NULL;
