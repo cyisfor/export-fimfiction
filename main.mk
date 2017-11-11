0180bab2a85de6b28ed8af013c209936d8b9176a
autocommit <autocommit>
2017-11-11 20:31:19 +0000

    auto 0 31 440

--- main.mk
+++ main.mk
@@ -1,52 +1,26 @@
-CFLAGS+=-ggdb -O2
+include coolmake/top.mk
+
+CFLAGS+=-ggdb
 CFLAGS+=-Io -Ihtml_when/src -Ilibxml2/include -Ilibxmlfixes/src -Ilibxmlfixes/o
 P=gtk+-3.0 glib-2.0 gio-2.0
 
-LDLIBS+=$(shell pkg-config --libs $P) -lpcre
 LDLIBS+=$(shell xml2-config --libs | sed -e's/-xml2//g')
 LINK=$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS)
 
-O=$(patsubst %,o/%.o,$N) libxml2/.libs/libxml2.a html_when/libhtmlwhen.a \
-	libxmlfixes/libxmlfixes.a \
-	$(foreach name,$(N),$(eval targets:=$$(targets) $(name)))
-
-derp: setup
-	$(MAKE) -C html_when
-	$(MAKE) export-fimfiction
+LIBS=libxml2/.libs/libxml2.a \
+	html_when/libhtmlwhen.a \
+	libxmlfixes/libxmlfixes.a
 
 N=gui wordcount main
-export-fimfiction: $O
-	$(LINK)
+OUT=export-fimfiction
+$(eval $(PROGRAM))
 
--include $(patsubst %,o/%.d,$N)
-
-o/main.o: libxml2/include/libxml/xmlversion.h 
+$(O)/main.lo: libxml2/include/libxml/xmlversion.h libxmlfixes/o/wanted_tags.gen.h
 
 libxml2/include/libxml/xmlversion.h libxml2/.libs/libxml2.a: libxml2/configure
 
-libxml2/configure:
+libxml2/configure: | libxml2
 	$(MAKE) -C html_when
 
-o:
-	mkdir o
-
-COMPILE=$(CC) $(CFLAGS) `pkg-config --cflags $(P)` -c -o $@ $<
-
-o/%.d: src/%.c | o
-	$(COMPILE) -MM
-
-o/%.o: src/%.c | o
-	$(COMPILE)
-
-o/main.d o/main.o: libxmlfixes/o/wanted_tags.gen.h
-
-clean:
-	rm -rf o
-
-.PHONY: all stuff setup derp
-
-setup:
-	sh setup.sh
-	$(MAKE) -C html_when setup
-
--include $(patsubst %, o/%.d,$(targets))
+libxml2: | html_when/libxml2
+	ln -rs $| $@
